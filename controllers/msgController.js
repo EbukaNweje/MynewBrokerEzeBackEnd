aa8aaafac33c534b7acda4267cd12bacdec37b72
ColdDdev <colindecorce@gmail.com>
2026-04-04 13:08:13 +0100

    D

--- controllers/msgController.js
+++ controllers/msgController.js
@@ -1,27 +1,25 @@
-const msgModel = require("../models/msgModel")
+const msgModel = require("../models/msgModel");
 
-exports.getNotification = async(req,res)=>{
-    try {
-        
-        // get the user,s id
-        const id = req.parama.id
+exports.getNotification = async (req, res) => {
+  try {
+    // get the user,s id
+    const { id } = req.params;
 
-        // find the history by the user's id
-        const notify = await msgModel.findOne(id)
-        if (!notify) {
-            return res.status(404).json({
-                message:"No available notification"
-            })
-        }
-
-        // return the history
-        res.status(200).json({
-            data: notify
-        })
-
-    } catch (err) {
-        res.status(500).json({
-            message:err.message
-        })
+    // find the history by the user's id
+    const notify = await msgModel.findOne({ id });
+    if (!notify) {
+      return res.status(404).json({
+        message: "No available notification",
+      });
     }
-}
+
+    // return the history
+    res.status(200).json({
+      data: notify,
+    });
+  } catch (err) {
+    res.status(500).json({
+      message: err.message,
+    });
+  }
+};
