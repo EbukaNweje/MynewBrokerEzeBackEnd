8b9b2ce3a37d03e330d335edf0b7d85616730456
ColdDdev <colindecorce@gmail.com>
2026-04-04 13:02:41 +0100

    D

--- routes/msgRouter.js
+++ routes/msgRouter.js
@@ -2,6 +2,6 @@ const router = require("express").Router();
 const { getNotification } = require("../controllers/msgController");
 const authorization = require("../middleware/authorization");
 
-router.get("/getallnotification", authorization, getNotification);
+router.get("/getallnotification/:id", authorization, getNotification);
 
 module.exports = router;
